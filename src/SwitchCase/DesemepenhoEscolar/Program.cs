0bc9c3ad5b268a0fe45d5bf6036162d4bba09338
Edilton <ediltonsaraiva@gmail.com>
2026-07-12 19:34:17 -0300

    feat: adiciona mini projetos de Switch Case

---
+++ src/SwitchCase/DesemepenhoEscolar/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics.Contracts;
+
+namespace DesemepenhoEscolar;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("--------DESEMPENHO ESCOLAR--------");
+
+        Console.WriteLine("Por favor, digite a sua nota:\t");
+        int SuaNota = Convert.ToInt32(Console.ReadLine());
+
+        switch (SuaNota) 
+        {
+            case 10:
+            case 9:
+                Console.WriteLine("\n-EXECELENTE!");
+                break;
+            case 8:
+            case 7:
+                Console.WriteLine("\n-MUITO BOM!");
+                break;
+            case 6:
+                Console.WriteLine("\n-REGULAR!");
+                break;
+            case 5:
+                Console.WriteLine("\n-INSATISFATÓRIO!");
+                break;
+            default:
+                Console.WriteLine("\n-RUIM!");
+                break;
+        }
+    }
+}
---
+++ src/SwitchCase/DiaDaSemana/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace DiaDaSemana;
+
+public class Program 
+{
+    public static void Main()
+    {
+        Console.WriteLine("------------------------DIA DA SEMANA------------------------");
+
+        Console.Write("\nDigite um número de 1 a 7 para saber o dia da semana correspondente:\t");
+        int DiaDaSemana = Convert.ToInt32(Console.ReadLine());
+
+        switch (DiaDaSemana) 
+        {
+            case 1:
+                Console.WriteLine("\n-Domingo");
+                break;
+            case 2:
+                Console.WriteLine("\n-Segunda-feira");
+                break;
+            case 3:
+                Console.WriteLine("\n-Terça-feira");
+                break;  
+            case 4:
+                Console.WriteLine("\n-Quarta-feira");
+                break;
+            case 5:
+                Console.WriteLine("\n-Quinta-feira");
+                break;
+            case 6:
+                Console.WriteLine("\n-Sexta-feira"); 
+                break;
+            case 7:
+                Console.WriteLine("\n-Sábado");
+                break;
+            default:
+                Console.WriteLine("\nDia INVÁLIDO! Por favor, digite um número de 1 a 7.");
+                break;
+        }
+    }
+}
