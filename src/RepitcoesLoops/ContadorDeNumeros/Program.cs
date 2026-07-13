c7ec8b74c512ac84822fe1b481796dfa7dedb7df
Edilton <ediltonsaraiva@gmail.com>
2026-07-13 17:56:10 -0300

    Adiciona diretório de repetições e loops

---
+++ src/RepitcoesLoops/ContadorDeNumeros/Program.cs
@@ -0,0 +1,92 @@
+﻿using System.Diagnostics;
+
+namespace ContadorDeNumeros;
+
+public class Program
+{
+    public static void Main()
+    { 
+        Console.WriteLine("------------CONTADOR DE NÚMEROS--------------");
+
+        Console.WriteLine("Ordens de contagem disponíveis:");
+        Console.WriteLine("\n1 - Crescente");
+        Console.WriteLine("2 - Decrescente\n");
+
+        Console.Write("Digite o NÚMERO da ordem de contagem desejada:\t");
+        int OrdemDeContagem = Convert.ToInt32(Console.ReadLine());
+
+        if (OrdemDeContagem != 1 && OrdemDeContagem != 2)
+        {
+            Console.WriteLine("\n-Opção INVÁLIDA! Escolha 1 para CRESCENTE ou 2 para DECRESCENTE.");
+        }
+        else 
+        {
+            Console.Write("Digite em que número deseja inicar a contagem:\t");
+            int NumeroDeContagem = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite em que número a contagem deve parar:\t");
+            int NumeroDeParada = Convert.ToInt32(Console.ReadLine());
+
+            bool CondicaoDeParada = false;
+            bool ErroDeOrdem = false;
+
+            Console.WriteLine();
+
+            switch (OrdemDeContagem)
+            {
+                case 1:
+                    while (CondicaoDeParada == false)
+                    {
+                        if (ErroDeOrdem == false)
+                        {
+                            if (NumeroDeContagem == NumeroDeParada)
+                            {
+                                Console.WriteLine(NumeroDeContagem);
+                                Console.WriteLine("\nContagem Finalizada!");
+                                break;
+                            }
+                            else if (NumeroDeContagem < NumeroDeParada)
+                            {
+                                Console.Write($"{NumeroDeContagem} ");
+                                NumeroDeContagem++;
+                            }
+                            else
+                            {
+                                ErroDeOrdem = true;
+                                Console.WriteLine("-Ordem CRESCENTE escolhida!\nO número de PARADA deve ser MAIOR que o número de INÍCIO.");
+                                break;
+                            }
+                        }
+                    }
+                    break;
+
+                case 2:
+                    while (CondicaoDeParada == false)
+                    {
+                        if (ErroDeOrdem == false)
+                        {
+                            if (NumeroDeContagem == NumeroDeParada)
+                            {
+                                Console.WriteLine(NumeroDeContagem);
+                                Console.WriteLine("\nContagem Finalizada!");
+                                break;
+
+                            }
+                            else if (NumeroDeContagem > NumeroDeParada)
+                            {
+                                Console.Write($"{NumeroDeContagem} ");
+                                NumeroDeContagem--;
+                            }
+                            else
+                            {
+                                ErroDeOrdem = true;
+                                Console.WriteLine("-Ordem DECRESCENTE escolhida!\nO número de PARADA deve ser MENOR que o número de INÍCIO.");
+                                break;
+                            }
+                        }
+                    }
+                    break;
+            }
+        }
+    }
+}
---
+++ src/RepitcoesLoops/FogosDeArtificio/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace FogosDeArtificio;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.Write("Digite o ano da explosão de fogos:\t");
+        int AnoDaExplosao = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine($"\n----------EXPLOSÃO DE FOGOS ANO {AnoDaExplosao}---------\n");
+
+        int NumeroDeContagem = 10;
+
+        Console.WriteLine("Contagem regressiva para a explosão de fogos:\n");
+
+        do
+        {
+            Console.Write($"{NumeroDeContagem}  ");
+            NumeroDeContagem--;
+        } while (NumeroDeContagem >= 0);
+
+        int ContagemDeFogos = 1;
+        Console.WriteLine("\n");
+
+        do
+        {
+            Console.WriteLine(".+.°.+´.º+.´´.º+.°.+´.º+.´´.º+.°.+´.º+.´´.º.");
+            ContagemDeFogos++;
+        } while (ContagemDeFogos <= 5);
+
+        Console.WriteLine($"\n                FELIZ {AnoDaExplosao + 1}!\n");
+
+    }
+}
