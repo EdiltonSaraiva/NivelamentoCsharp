4b62d9f243c47625f8a657783ce584fd07cf2252
Edilton <ediltonsaraiva@gmail.com>
2026-08-02 20:12:10 -0300

    feat: adiciona SomaSubrtrai em ProgramacaoOrientadaObjetos

---
+++ src/ProgramacaoOrientadaObjetos/SomaSubtrai/Calculadora.cs
@@ -0,0 +1,15 @@
+﻿namespace SomaSubtrai;
+
+class Calculadora
+{
+    public static void Somar(int primeiroNumero, int segundoNumero)
+    {
+        Console.WriteLine($"A soma de {primeiroNumero} + {segundoNumero} = {primeiroNumero + segundoNumero}");
+    }
+
+    public static void Subtrair(int primeiroNumero, int segundoNumero)
+    {
+        Console.WriteLine($"A subtração de {primeiroNumero} - {segundoNumero} = {primeiroNumero - segundoNumero}");
+    }
+}
+
---
+++ src/ProgramacaoOrientadaObjetos/SomaSubtrai/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SomaSubtrai;
+
+public class Program
+{
+    public static void Main()
+    {
+        Calculadora.Somar(10, 15);
+        Calculadora.Subtrair(10, 15);
+    }
+}
